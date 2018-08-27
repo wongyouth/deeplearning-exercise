497fe297bf5f03f83a1c00b9941496feb8d95d88
Ryan Wang <wongyouth@gmail.com>
2018-08-27 13:52:22 +0800

    support positive and negative case

--- checkline/dataset.py
+++ checkline/dataset.py
@@ -1,6 +1,8 @@
 import numpy as np
+from skimage.draw import line_aa
 import random
 
+np.set_printoptions(linewidth=300)
 random.seed(42)
 
 ##
@@ -13,27 +15,72 @@ random.seed(42)
 # @param delta {int} the max distance of the next point to previous point,
 #                    if DELTA value is larger than STROKEWIDTH the line may become discontinuous
 def lineBox(positive = True, w = 28, h = 28, strokewidth = 3, delta = 1):
-    data = np.zeros((h, w))
-
-    if not positive:
-        # get segmental line as negative line
-        offset = random.randint(0, h // 2)
-        length = random.randint(1, h // 2)
-        zone = range(offset, offset + length)
-
-        for (x,y) in line(w, delta, h-1):
-            if y in zone:
-                width = random.randint(1, strokewidth)
-                start = max(x - width // 2, 0)
-                data[y][start:start+width] = 255
+    if random.random() > 0.5:
+        data = createBox1(w, h)
     else:
-        for (x,y) in line(w, delta, h-1):
-            width = random.randint(1, strokewidth)
-            start = max(x - width // 2, 0)
-            data[y][start:start+width] = 255
+        data = createBox2(w, h, strokewidth, delta)
+
+    if positive:
+        return data
+    else:
+        if random.random() > 0.5:
+            data = negative1(w, h, data)
+        else:
+            data = negative2(w, h, data)
+
+        return data
+
+# the length of line is less than 50%
+def negative1(w, h, data):
+    data2 = np.zeros((h, w), dtype=np.uint8)
+
+    # get segmental line as negative line
+    offset = random.randint(0, h // 2)
+    length = random.randint(1, h // 2)
+    zone = range(offset, offset + length)
+
+    data2[zone, :] = 1
+    data = data2 * data
 
     return data
 
+# cross line case
+def negative2(w, h, data):
+    y_start = random.randint(0, h-1)
+    y_end = random.randint(0, h-1)
+
+    index = np.nonzero(data[y_start])[0][0]
+    x_start = random.randint(0, max(0, index-2))
+
+    index = np.nonzero(data[y_end])[0][-1]
+    x_end = random.randint(min(w-1, index + 2), w-1)
+
+    rr, cc, val = line_aa(y_start, x_start, y_end,  x_end)
+    data[rr, cc] = val * 255
+
+    return data
+
+# anti-alias line
+def createBox1(w = 28, h = 28):
+    data = np.zeros((h, w), dtype=np.uint8)
+
+    start = random.randint(0, w-1)
+    end = random.randint(0, w-1)
+
+    rr, cc, val = line_aa(0, start, h - 1, end)
+    data[rr, cc] = val * 255
+
+    return data
+
+def createBox2(w = 28, h = 28, strokewidth = 3, delta = 1):
+    data = np.zeros((h, w), dtype=np.uint8)
+
+    for (x,y) in line(w, delta, h-1):
+        width = random.randint(1, strokewidth)
+        start = max(x - width // 2, 0)
+        data[y][start:start+width] = 255
+
+    return data
 
 # line data
 def line(w, delta, y):
@@ -53,7 +100,6 @@ def line(w, delta, y):
         x = min(x, w-1)
         prev.append((x,y))
         return prev
-
 
 def load_data(size):
     positive = size // 10 * 6
